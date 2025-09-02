36bc771c1100b30d5d8ba4de6431aa3ff3085657
Salah-din <salahdinbenaouda@gmail.com>
2025-09-02 16:15:12 +0200

    fix: path fixed for Login -> LoginView

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/pages/Login.vue'
+import Login from '@/pages/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
