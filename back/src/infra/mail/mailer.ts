5a543c1fa517e2c41f74c622a1277d9bc2a5252e
Salah-din <salahdinbenaouda@gmail.com>
2025-09-30 14:42:11 +0200

    fix(mailer): use only onboarding email

--- back/src/infra/mail/mailer.ts
+++ back/src/infra/mail/mailer.ts
@@ -2,7 +2,7 @@ import { Resend } from 'resend';
 import { env } from '../../config/env.js';
 
 const resend = new Resend(env.RESEND_API_KEY);
-const from = env.MAIL_FROM || 'onboarding@resend.dev';
+const from = /* env.MAIL_FROM ||  */'onboarding@resend.dev';
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
     const result = await resend.emails.send({
